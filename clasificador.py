62fe541d1afeb956011ee385a24f52ca8dd1dcaf
Kosho969 <pablo.koch26@gmail.com>
2018-03-19 12:00:10 -0600

    Added classify and fixed some bugs

---
+++ clasificador.py
@@ -0,0 +1,95 @@
+from collections import Counter
+import functools
+
+class Clasificador():
+
+    SPAM_MESSAGE = 1;
+
+    HAM_MESSAGE = 0;
+
+    def __init__(self, ham_messages_list, spam_messages_list):
+        self.ham_messages_list = ham_messages_list
+        self.spam_messages_list = spam_messages_list
+        self.ham_words_frequence_dictionary = self.generate_word_bank(ham_messages_list)
+        self.spam_words_frequence_dictionary = self.generate_word_bank(spam_messages_list)
+
+    def generate_word_bank(self, messages):
+        words = []
+
+        for message in messages:
+            words.extend(message.split(' '))
+
+        return Counter(words)
+
+    '''
+    Returns the ocurrences of word in the dictionary defined by type (ham or spam)
+    '''
+    def count_word_ocurrences_of_type(self, word, type):
+        if (type == self.SPAM_MESSAGE):
+            return self.spam_words_frequence_dictionary[word]
+        else:
+            return self.ham_words_frequence_dictionary[word]
+
+    '''
+    Returns the amount of words (with their ocurrences) in the dictionary defined by type
+    '''
+    def count_total_ocurrences_of_type(self, type):
+        if (type == self.SPAM_MESSAGE):
+            return sum(self.spam_words_frequence_dictionary.values())
+        else:
+            return sum(self.ham_words_frequence_dictionary.values())
+
+    '''
+    Returns the amount of distinct words both in spam and ham
+    '''
+    def count_distinct_words(self):
+        return len(self.ham_words_frequence_dictionary + self.spam_words_frequence_dictionary)
+
+    def get_messages_list(self, type):
+        if (type == self.SPAM_MESSAGE):
+            return self.spam_messages_list
+        else:
+            return self.ham_messages_list
+
+    def get_count_messages(self):
+        return len(self.spam_messages_list) + len(self.ham_messages_list)
+
+    '''
+    Return the probability of a message being that word, given message is of
+    type type (total probability)
+    '''
+    def get_probability_of_word(self, message, type, k):
+        # Whats the probability of any message of being of type 'type'
+        if (message == None):
+            return (len(self.get_messages_list(type)) + k) / (self.get_count_messages() + k * 2)
+
+        return (
+            (self.count_word_ocurrences_of_type(message, type) + k)
+                / (
+                    self.count_total_ocurrences_of_type(type)
+                        + k * self.count_distinct_words()
+                )
+        )
+
+    def get_inverse_type(self, type):
+        if (type == self.SPAM_MESSAGE):
+            return self.HAM_MESSAGE
+        else:
+            return self.SPAM_MESSAGE
+
+    def classify_message(self, message, type, k):
+        N = message.split(' ')
+
+        # Multiplicatoria_{p in N} (clasificar(p, X)) * clasificar(null, X, K)
+        upperFactor = functools.reduce(
+            lambda x, y: x * y,
+            [self.get_probability_of_word(word, type, k) for word in N]
+        ) * self.get_probability_of_word(None, type, k)
+
+        # Multiplicatoria_{p in N} (clasificar(p, !X)) * clasificar(null, !X, K)
+        lowerFactor = functools.reduce(
+            lambda x, y: x * y,
+            [self.get_probability_of_word(word, self.get_inverse_type(type), k) for word in N]
+        ) * self.get_probability_of_word(None, self.get_inverse_type(type), k)
+
+        return upperFactor / (upperFactor + lowerFactor)
--- training.py
+++ training.py
@@ -1,12 +1,23 @@
 import re
 import random
+import sys
+from clasificador import Clasificador
 
+'''
+Sanitization involves removing all non-letter characters and removing
+words consisting in one only letter
+'''
 def sanitize_classified_messages(messages):
     regex = re.compile('[^a-zA-Z]')
+
     for i in range (len(messages)):
-        messages[i] = regex.sub(' ',messages[i])
-        messages[i] = re.sub(' +',' ',messages[i])
-        messages[i] = ' '.join( [w for w in messages[i].split() if len(w)>1] )
+        messages[i] = regex.sub(' ', messages[i])
+        messages[i] = re.sub(' +', ' ', messages[i])
+        # TODO: convert message to lowercase
+        messages[i] = ' '.join([
+            w for w in messages[i].split() if len(w) > 1
+        ])
+
     return messages
 
 def debug_messages(messages):
@@ -16,18 +27,17 @@ def debug_messages(messages):
         print("\n")
 
 def remove_elements(messages,index):
-    del messages[0:index+1]
+    del messages[0:index + 1]
     return messages
-
-def 
 
 ham_messages = []
 spam_messages = []
 
 with open('corpus.txt') as f:
     for line in f: 
-        print(line, end='')
+        # print(line, end='')
         value = line.split('\t')
+
         if value[0] == 'ham':
             ham_messages.append(value[1])
         else:
@@ -39,38 +49,71 @@ ham_messages = sanitize_classified_messages(ham_messages)
 random.shuffle(spam_messages)
 random.shuffle(ham_messages)
 
-print(len(ham_messages))
+# print(len(ham_messages))
 
-# TODO :  Optimize with method 
-training_percentage = round(len(ham_messages)*0.8)
+# TODO:  Optimize with method 
 
+# Build ham messages list
+training_percentage = round(len(ham_messages) * 0.8)
 ham_training_messages = [ham_messages[i] for i in range(training_percentage)]
-ham_messages = remove_elements(ham_messages,training_percentage)
+ham_messages = remove_elements(ham_messages, training_percentage)
 
-cv_percentage = round(len(ham_messages)/2)
+cv_percentage = round(len(ham_messages) / 2)
 ham_cross_validation_messages = [ham_messages[i] for i in range(cv_percentage)]
 ham_messages = remove_elements(ham_messages,cv_percentage)
-
 ham_test_messages = ham_messages
 
-training_percentage = round(len(spam_messages)*0.8)
-
+# Build spam messages list
+training_percentage = round(len(spam_messages) * 0.8)
 spam_training_messages = [spam_messages[i] for i in range(training_percentage)]
 spam_messages = remove_elements(spam_messages,training_percentage)
 
-cv_percentage = round(len(spam_messages)/2)
+cv_percentage = round(len(spam_messages) / 2)
 spam_cross_validation_messages = [spam_messages[i] for i in range(cv_percentage)]
 spam_messages = remove_elements(spam_messages,cv_percentage)
-
 spam_test_messages = spam_messages
 
-print(len(ham_training_messages))
-print(len(ham_cross_validation_messages))
-print(len(ham_test_messages))
+# clasificador = Clasificador(ham_training_words, spam_training_words)
 
+clasificador = Clasificador(
+    ['play sports today', 'went play sports', 'secret sports event', 'sports is today', 'sports cost money'],
+    ['offer is secret', 'click secret link', 'secret sports link']
+)
 
-print(len(spam_training_messages))
-print(len(spam_cross_validation_messages))
-print(len(spam_test_messages))
+# TODO: Implementar clasificador.get_performance(test_messages_dict, k)
+#   test_messages_dict: [
+#     ('Test message 1', spam),
+#     ('Test message 1', spam),
+#     ('Test message 1', spam),
+#     ('Test dsa 1', ham),
+#     ('Test message', ham),
+#     ('Test mesadfgasd 1', ham),
+#   ]
+#
+#   1. Clasificar cada mensaje, agregar la lista P 1 si acerto con type, y 0 si no acerto
+#   2. retornar la suma de valores divido el total de elementos
+#       [1, 0, 0, 0, 1, 0]: Performance: 2 / 6
 
+# print(clasificador.getPerformance(['ham test message 1', 'ham test message 2'], ham, 1))
+# print(clasificador.getPerformance(['spam test message 1', 'spam test message 2'], spam, 1))
 
+# TODO (forma chafa): Definir un listado de 10 valores diferentes para k
+#   Iterar sobre cada valor de k, obteniendo el performance para spam y ham
+#   Devolver el K que genere los valores más altos de spam y ham
+
+# TODO get_best_k_performance(x1, x2, iteracion):
+#   0. Si iteracion == maxIteraciones:
+#       if (clasificador.getPerformance(messagesList, x1) > clasificador.getPerformance(messagesList, x2))
+#           return x1
+#       else
+#           return x2
+#
+#   1. Calcular un x3 intermedio (promedio de x1 y x2)
+# 
+#   2. Computar clasificador.getPerformance(messagesList, P) para [x1, x2, x3]
+# 
+#   3. Si los dos mas grandes son x1 y x3, retornar get_best_k_performance(x1, x3, iteracion + 1)
+# 
+#   4. Si los dos más grandes son x2 y x3, retornar get_best_k_performance(x2, x3, iteracion + 1)
+# 
+#   5. Si los dos más grandes son x1 y x2, reteornar get_best_k_performance(max(x1, x2), x3, iteracion + 1)
