fa43350063b0579efcfbb341cb885d66f17ee4fa
Kosho969 <pablo.koch26@gmail.com>
2018-04-11 14:03:47 -0600

    Final

--- classify.py
+++ classify.py
@@ -7,9 +7,14 @@ from general_functions import *
 ham_training_messages = convert_to_list('train_ham.txt')
 spam_training_messages = convert_to_list('train_spam.txt')
 
-original_messages = convert_to_list('test_file.txt')
+
+input_file = 'test_file.txt'
+k = 0.48100000000000026
+
+
+original_messages = convert_to_list(input_file)
 messages_to_classify = sanitize_classified_messages(original_messages)
-original_messages = convert_to_list('test_file.txt')
+original_messages = convert_to_list(input_file)
 
 print(original_messages[-1])
 print(messages_to_classify[-1])
@@ -18,7 +23,7 @@ clasificador = Clasificador(ham_training_messages, spam_training_messages)
 
 results = []
 for i in range(len(messages_to_classify)):
-    probability = clasificador.classify_message(messages_to_classify[i], clasificador.SPAM_MESSAGE, 0.001)
+    probability = clasificador.classify_message(messages_to_classify[i], clasificador.SPAM_MESSAGE, k)
     if probability >= 0.7:
         results.append('spam\t{}'.format(original_messages[i]))
     else:
